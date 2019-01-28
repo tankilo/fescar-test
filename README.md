fcf701fa0cf87dbaef9c20b550aff0523076f422
tankilo <tankilo@126.com>
2019-01-28 15:12:45 +0800

    simple 单进程 TM操作两个RM

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # fescar-test
 这个项目修改[阿里云 GTS的样例工程](https://help.aliyun.com/document_detail/57267.html)    
 来测试GTS的开源版本 [fescar](https://github.com/alibaba/fescar)
+
+## fescar-test-simple
+为了测试方便我把两个数据源都指向了相同的mysql     
+执行fescar-simple.sql和undo_log.sql     
+运行com.tankilo.Simple    
---
+++ fescar-test-simple/src/main/resources/fescar-simple.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `user_money_a` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `money` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_money_b` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `money` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
